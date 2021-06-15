14a36ab25cf4eff134c7e867e3dca8dc309f102f
HamazakiHisanori <hiii1023@icloud.com>
2021-06-15 16:03:54 +0900

    ビューの細かな修正

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -40,10 +40,14 @@ export default function MemoDetailScreen(props) {
       </View>
 
       <View>
-        <ScrollView style={styles.memoBody}>
-          <Text style={styles.memoText}>
-            {memo && memo.bodyText}
-          </Text>
+        <ScrollView>
+
+          <View style={styles.memoBodyInner}>
+            <Text style={styles.memoText}>
+              {memo && memo.bodyText}
+            </Text>
+          </View>
+
         </ScrollView>
       </View>
 
@@ -86,8 +90,9 @@ const styles = StyleSheet.create({
     fontSize: 12,
     lineHeight: 16,
   },
-  memoBody: {
-    paddingVertical: 32,
+  memoBodyInner: {
+    paddingTop: 32,
+    paddingBottom: 80,
     paddingHorizontal: 27,
   },
   memoText: {
